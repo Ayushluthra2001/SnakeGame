503502addeae5909b20ac43b1bf2078efd122437
Ayush Luthra <ayushluthra62@gmail.com>
2022-11-20 00:24:06 +0530

    Files Updated

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 
 // Constant and variables declearation 
 
-// initial dirrection defined 
+// initial direction defined 
 let inputDir = { x: 0, y: 0 };
 
 // adding sounds
@@ -9,11 +9,13 @@ const foodSound = new Audio('./music/food.mp3');
 const gameOverSound = new Audio('./music/gameover.mp3');
 const moveSound = new Audio('./music/move.mp3');
 const musicSound = new Audio('./music/music.mp3');
+// variable and constant 
 let speed = 5;
 let lastPaintTime = 0;
 let snakeArr = [{ x: 13, y: 15 }];
 food = { x: 6, y: 7 }; // food is an object as food is an single particle and snake is array because has lot of element when he eat more food  
 let score=0;
+let hiscoreval;
 
 // Game Function 
 function main(currentTime) {
@@ -31,7 +33,7 @@ function isCollide(snakeArr){
             return true;
         }
     }
-        // if you bump into the wall 
+        // if your snake bump into the wall 
         if(snakeArr[0].x>=18 || snakeArr[0].x<=0 || snakeArr[0].y>=18 || snakeArr[0].y<=0){
             return true;
         }
@@ -39,7 +41,7 @@ function isCollide(snakeArr){
 }
 
 function gameEngine() {
-    // part 1  : updating the snake array that contain locataion of snake body part
+    // part 1  : updating the snake array that contain location of snake body part.
 
     if(isCollide(snakeArr)){
         gameOverSound.play();
@@ -49,6 +51,7 @@ function gameEngine() {
         speed=5;
         scoreBox.innerHTML="Score: "+score; 
         alert("Gmae Over. Press any key to play again!");
+        
         snakeArr=[{x:13,y:15}];
         musicSound.play();
         
@@ -64,6 +67,7 @@ function gameEngine() {
             localStorage.setItem("hiscore",JSON.stringify(hiscoreval));
             hiscorebox.innerHTML= "Hi Score:  "+hiscoreval;
         }
+        // speed increasing 
         if(score>=10){
             if(score%2==0) speed+=1;
         }
@@ -80,7 +84,7 @@ function gameEngine() {
     }
 
     // moving the snake 
-
+    
     for(let i=snakeArr.length-2;i>=0;i--){
         
         snakeArr[i+1]={...snakeArr[i]}; // we can't write it as snakearr[i+1]=snakeArr[i] because it will not work 
@@ -121,7 +125,7 @@ function gameEngine() {
 let hiscore=localStorage.getItem("hiscore");
 
 if(hiscore===null){
-     hiscoreval=0;
+    hiscoreval=0;
      localStorage.setItem("hiscore",JSON.stringify(hiscoreval))
 }else{
     hiscoreval=JSON.parse(hiscore);
